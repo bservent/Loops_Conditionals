a3ef4c4c2d3a9c7a4c9d9270396fd28152226452
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-09-12 15:46:55 -0700

    Commit 2 - Get Even answered

--- script/app.js
+++ script/app.js
@@ -1,11 +1,21 @@
 /* console.log('HW-1')
 
+
 Easy Going
 1. Write a for loop that will log the numbers 1 through 20. */
 
 /* for (i = 1; i <= 20; i+=1) {
     console.log(i);
 } */
+
+
+/* Get Even
+1. Write a `for` loop that will log only the even numbers in 0 through 200.
+>Hint: Think about the increment expression. */
+
+/* for (i = 0; i <= 200; i+=2) {
+        console.log(i);
+    } */
 
 
 
